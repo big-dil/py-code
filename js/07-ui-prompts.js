963d66c4350320fccef917b88a8ddf052852f696
big-dil <git.dev@hotmail.com>
2025-12-28 14:25:44 +0200

    Update 07-ui-prompts.js

--- js/07-ui-prompts.js
+++ js/07-ui-prompts.js
@@ -1,1 +1,29 @@
+function promptAddFullBuilding() { /* ... */ }
+function promptAddFloor(buildingId) { /* ... */ }
+function promptAddRoom(floorId) { /* ... */ }
+function promptAddBed(roomId) { /* ... */ }
+function promptEditBuilding(id, name) { /* ... */ }
+function promptEditFloor(id, num, display) { /* ... */ }
+function promptEditRoom(id, num) { /* ... */ }
 
+function promptBulkCreate(type, parentId, label, placeholder, logicFn) {
+    showModal(`הוספת ${type}`, `<div class="row"><input id="in-bulk" placeholder="${placeholder}"/></div>`, () => {
+        const res = logicFn(parentId, el('#in-bulk').value);
+        showModal('סיכום', `נוספו: ${res.added}`, null, 'סגור');
+    });
+}
+
+function promptAssign(bedId) {
+    // לוגיקת שיבוץ עם Select וחיפוש...
+    // ...
+}
+
+function promptEditSelectedStudent() {
+    if (!selectedStudentId) return;
+    const s = findStudent(selectedStudentId);
+    // בניית טופס עריכה גדול...
+    // ...
+}
+
+function promptResetOptions() { /* ... */ }
+function promptExportType() { /* ... */ }
